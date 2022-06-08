68aa572d3f940fe488ff8dd4c745b0b84c8431f9
siddhu4508 <siddhuyadav508@gmail.com>
2022-06-08 23:58:09 +0530

    commit

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,29 +1,69 @@
+from msilib.schema import Class
+from random import choices
 from socket import socket
+from unicodedata import decimal
 from django.db import models
+
+MANUFACTURER_CHOICES = [
+    ('IN', 'Intel'),
+    ('AM', 'AMD'),
+]
+
+CORE_CHOICES = [
+    ('1', '1'),
+    ('2', '2'),
+    ('4', '4'),
+    ('6', '6'),
+    ('8', '8'),
+    ('10', '10'),
+    ('12', '12'),
+    ('14', '14'),
+    ('16', '16'), 
+    ('18', '18'),  
+]
+
+CODE_NAME_CHOICES = [
+    ('AL', 'Alder Lake'),
+    ('RL','Rocket Lake'),
+    ('SB','Sandy Bridge'),
+    ('IB','Ivy Bridge'),
+    ('H','Haswell'),
+    ('B','Broadwell'),
+    ('SL','Skylake'),
+    ('CL','Coffe Lake'),
+    ('KL','Kaby Lake'),
+    ('GL','Gemini Lake'),
+    ('WL','Whiskey Lake'),
+    ('TL','Tiger Lake'),
+    ('LF','Lakefield'),
+    ('CL','Cascade Lake'),
+    ('CO','Comet Lake'),
+    ('RL','Rocket Lake'),
+    ('D','Denverton'),
+   
+]
+
 
 class Product(models.Model):
     name = models.CharField(max_length=120) 
     img = models.ImageField(blank=True, null=True)
-    cores = models.IntegerField()
+    cores = models.CharField(max_length=2, choices=CORE_CHOICES)
     core_clock = models.FloatField()
     boost_clock = models.FloatField(blank=True, null=True)
-    tdp = models.DecimalField(decimal_places=3, max_digits=100)
+    tdp = models.IntegerField()
     integrated_graphics = models.BooleanField(("yes"))  
     price = models.FloatField()
-    manufacture = models.CharField(max_length=120)
+    manufacturer = models.CharField(max_length=2, choices=MANUFACTURER_CHOICES)
     micro_architecture = models.CharField(max_length=50)
-    core_family = models.CharField(max_length=40)
+    code_name = models.CharField(max_length=2,choices=CODE_NAME_CHOICES)
     socket_name = models.CharField(max_length=40)
     maximum_supported_memory = models.IntegerField()
     performance = models.CharField(max_length=30)
-    lithograph = models.CharField(max_length=30)
+    lithography = models.CharField(max_length=30)
     cpu_cooler = models.CharField(max_length=3)
-    
-    
-    
-    
-    
 
     def __str__(self):
         return self.name
+
+
     
