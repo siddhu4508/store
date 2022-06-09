f1d924e33cc914fa8ea3447318306241198a0f18
siddhuyadav508@gmail.com <siddhuyadav508@gmail.com>
2022-06-10 02:25:55 +0530

    added static settings

--- shop/ecom/settings.py
+++ shop/ecom/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -120,3 +121,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
