4e7228db07f19c284b15238a28f4b2b5d9fc8de4
siddhuyadav508@gmail.com <siddhuyadav508@gmail.com>
2022-06-10 10:47:05 +0530

    added new model classes

--- shop/products/admin.py
+++ shop/products/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Cpu_cooler, Mother_board
 
 admin.site.register(Product)
+admin.site.register(Cpu_cooler)
+admin.site.register(Mother_board)
--- shop/products/models.py
+++ shop/products/models.py
@@ -1,6 +1,8 @@
 from msilib.schema import Class
 from random import choices
 from socket import socket
+from tkinter.messagebox import YES
+from turtle import heading
 from unicodedata import decimal
 from django.db import models
 
@@ -52,18 +54,91 @@ class Product(models.Model):
     boost_clock = models.FloatField(blank=True, null=True)
     tdp = models.IntegerField()
     integrated_graphics = models.BooleanField(("yes"))  
-    price = models.FloatField()
+    price = models.IntegerField()
+    rating = models.IntegerField(max_length=5)
     manufacturer = models.CharField(max_length=2, choices=MANUFACTURER_CHOICES)
+    part_number = models.CharField(max_length=40)
     micro_architecture = models.CharField(max_length=50)
     code_name = models.CharField(max_length=2,choices=CODE_NAME_CHOICES)
     socket_name = models.CharField(max_length=40)
     maximum_supported_memory = models.IntegerField()
-    performance = models.CharField(max_length=30)
+    performance_L1_cache = models.CharField(max_length=30)
+    performance_L2_cache = models.CharField(max_length=30)
+    performance_L3_cache = models.CharField(max_length=30)
     lithography = models.CharField(max_length=30)
     cpu_cooler = models.CharField(max_length=3)
+    simultaneous_multiprocessor= models.BooleanField((YES))
 
     def __str__(self):
         return self.name
+    
+class Cpu_cooler(models.Model):
+    name = models.CharField(max_length=30)
+    img = models.ImageField
+    fanRpm = models.IntegerField()
+    noise_level = models.IntegerField()
+    color = models.CharField(max_length=20)
+    radiator_size = models.IntegerField()
+    rating = models.IntegerField()
+    price = models.IntegerField()
+    manufacturer = models.CharField(max_length=40)
+    model = models.CharField(max_length=50)
+    part_number = models.CharField(max_length=40)
+    height = models.IntegerField()
+    Cpu_socket_supported = models.CharField(max_length=40)
+    water_cooled = models.BooleanField('yes')
+    fan_less = models.BooleanField('yes')
+    ecc_support = models.BooleanField
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Mother_board(models.Model):
+    name = models.CharField(max_length=30)
+    img = models.ImageField()
+    socket_cpu = models.CharField(max_length=40)
+    pcie_x_16_slots = models.IntegerField()
+    pcie_x_8_slots = models.IntegerField()
+    pcie_x_4_slots = models.IntegerField()
+    pcie_x_1_slots = models.IntegerField()
+    pcie_slots = models.IntegerField()
+    form_factor = models.CharField(max_length=20)
+    memory_max = models.IntegerField()
+    memory_slots = models.IntegerField()
+    color = models.CharField(max_length=10)
+    rating = models.IntegerField(max_length=5)
+    price = models.IntegerField()
+    manufacturer = models.CharField(max_length=40)
+    part_number = models.CharField(max_length=40)
+    chip_set = models.CharField(max_length=30)
+    memory_type = models.CharField(max_length=23)
+    memory_speed = models.IntegerField()
+    sli_cross_fire = models.BooleanField('yes')
+    m2_slots = models.IntegerField()
+    mini_Pcie_slots = models.IntegerField()
+    half_mini_pcie_mSATA_slots = models.IntegerField()
+    mSATA_slots = models.IntegerField()
+    SATA_speed = models.IntegerField()
+    on_board_ethernet = models.BooleanField()
+    on_board_video = models.BooleanField()
+    usb_2_headers = models.IntegerField()
+    usb_2_headers_single = models.IntegerField()
+    usb_3_2_gen_1_headers = models.IntegerField()
+    usb_3_2_gen_2_headers = models.IntegerField()
+    usb_3_gen_2x2_headers = models.IntegerField()
+    ecc_support = models.BooleanField()
+    wireless_networking = models.BooleanField()
+    Raid_support = models.BooleanField()
+    
+    
+    
+    
+    
+        
+    
+        
+    
 
 
     
